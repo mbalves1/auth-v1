78e64bcf1b33d461bb5c9d061a8ae74609d7778d
Murilo <mbalves1@outlook.com>
2025-02-12 12:28:17 -0300

    `feat: updated layout/default.vue with new navigation menu design`

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,15 +23,30 @@
     </div>
     <div class="flex">
       <div>
-        <div class="z-40 h-screen p-2 pt-4 overflow-y-auto transition-all duration-300 ease-in-out w-20 dark:bg-gray-900" :class="{ 'w-60 p-4': openMenu }">
-          <p @click="open" class="inline-block border rounded p-1">MyApp</p>
-          <nav>
-            <ul v-for="(nav, i) in navs" :key="i">
-              <li @click="goTo(nav.path)">
-                {{ nav.name }}
+        <div
+          class="z-40 h-screen p-2 pt-4 overflow-y-auto transition-all duration-300 ease-in-out w-20 dark:bg-gray-900 flex flex-col items-center justify-between"
+          :class="{ 'w-[170px] p-4': openMenu }">
+          <p @click="open" class="inline-block border rounded p-1"><LucideWalletMinimal /></p>
+          <nav class="space-y-2">
+            <ul v-for="(nav, i) in navs" :key="i" class="hover:bg-gray-800 hover:text-primary rounded cursor-pointer">
+              <li
+                @click="goTo(nav.path, i)"
+                class="flex items-center p-2 space-x-2 rounded"
+                :class="{ 'bg-gray-800 text-primary': activeIndex === i }">
+                <LucideHouse v-if="nav.icon === 'LucideHouse'"></LucideHouse>
+                <LucideLayoutDashboard v-if="nav.icon === 'LucideLayoutDashboard'"></LucideLayoutDashboard>
+                <LucideSheet v-if="nav.icon === 'LucideSheet'"></LucideSheet>
+                <LucideArrowRightLeft v-if="nav.icon === 'LucideArrowRightLeft'"></LucideArrowRightLeft>
+                <LucideUserRoundPen v-if="nav.icon === 'LucideUserRoundPen'"></LucideUserRoundPen>
+                <transition name="fade">
+                  <div v-show="openMenu">
+                    {{ nav.name }}
+                  </div>
+                </transition>
               </li>
             </ul>
           </nav>
+          <p class="h-[300px]"></p>
         </div>
       </div>
       <slot />
@@ -43,22 +58,33 @@
   const router = useRouter();
 
   const openMenu = ref(true);
+  const activeIndex = ref(null)
   const navs = ref([
-    { name: 'Home', path: '/home', icon: 'LucideMail' },
-    { name: 'Dashboard', path: '/dashboard' },
-    { name: 'Tabelas', path: '/tabelas' },
-    { name: 'Transações', path: '/transacoes' },
-    { name: 'Profile', path: '/profile' },
+    { name: 'Home', path: '/home', icon: 'LucideHouse' },
+    { name: 'Dashboard', path: '/dashboard', icon: 'LucideLayoutDashboard' },
+    { name: 'Tabelas', path: '/tabelas', icon: 'LucideSheet' },
+    { name: 'Transações', path: '/transacoes', icon: 'LucideArrowRightLeft' },
+    { name: 'Profile', path: '/profile', icon: 'LucideUserRoundPen' },
   ])
 
   function open() {
     return openMenu.value = !openMenu.value
   }
 
-  function goTo(params) {
+  function goTo(params, index) {
+    activeIndex.value = index
     router.push(params);
   }
 </script>
+<style>
+.fade-enter-active {
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+}
+.fade-enter-from {
+  opacity: 0;
+  transform: translateY(10px);
+}
+</style>
 <!-- #012A35 -->
 <!-- #001E26 -->
 <!-- #17323A -->
